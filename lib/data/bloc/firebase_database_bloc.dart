6643f04c36f16090a98af0fda9245516c73ef1f0
Korben Rusek <korben.rusek@gmail.com>
2018-11-08 21:09:35 -0800

    firebase data

--- lib/data/bloc/firebase_database_bloc.dart
+++ lib/data/bloc/firebase_database_bloc.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/widgets.dart';
 import 'package:reminder/data/bloc/database_bloc.dart';
+import 'package:reminder/data/firestore_provider.dart';
+import 'package:reminder/data/model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:uuid/uuid.dart';
 
 class FirebaseDatabaseBloc extends DatabaseBloc {
-  final String uuid;
+  FirestoreBloc _firestore;
+  String uuid;
+  String get _remindersPath => this.uuid.length > 0 ? "data/$uuid/reminders/" : "reminders";
+
   FirebaseDatabaseBloc({this.uuid});
 
   Future _loader;
@@ -14,7 +21,33 @@ class FirebaseDatabaseBloc extends DatabaseBloc {
   }
 
   Future _load(BuildContext context) async {
+    if (this.uuid == null) {
+      final prefs = await SharedPreferences.getInstance();
+      this.uuid = prefs.getString("uuid") ?? Uuid().v1();
+      await prefs.setString("uuid", this.uuid);
+    }
+
+    this._firestore = FirestoreProvider.of(context);
+    await _firestore.loader(context);
     return;
+  }
+
+  // TODO: implement remindersStream
+  @override
+  Stream<List<ReminderBase>> get remindersStream => this._firestore.instance.collection(this._remindersPath).snapshots().map(
+    (snapshot) {
+      return snapshot.documents.map((document) {
+        final json = document.data;
+        json["id"] = document.reference.path;
+        final ReminderBase reminder = Reminder.fromJson(json);
+        return reminder;
+      }).toList();
+    }
+  );
+
+  @override
+  Future update({ReminderBase reminder}) {
+    return this._firestore.instance.document(reminder.id).setData(reminder.toJson());
   }
 
 }
