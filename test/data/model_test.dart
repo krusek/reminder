d507f331ad89db0eb8173cb0d55ae542fd9d36b0
Korben Rusek <korben.rusek@gmail.com>
2018-11-28 19:48:24 -0800

    add some tests
    I'm just learning how to write tests, so they aren't great.

--- test/data/model_test.dart
+++ test/data/model_test.dart
@@ -6,7 +6,7 @@ import 'package:reminder/data/model.dart';
 import 'package:uuid/uuid.dart';
 
 void main() {
-  test("Test model serialization and deserialization", () async {
+  test("Test model with timespan serialization and deserialization", () async {
     final model = Reminder(
       id: Uuid().v1(), 
       title: Uuid().v1(), 
@@ -17,15 +17,43 @@ void main() {
         lastEvent: DateTime.now(),
       )
     );
+
     final json = model.toJson();
     final parsed = Reminder.fromJson(json);
+
     expect(model.id, parsed.id);
     expect(model.title, parsed.title);
     expect(model.description, parsed.description);
+    expect(model.frequency is Timespan, true);
     final modelFrequency = model.frequency as Timespan;
     final parsedFrequency = parsed.frequency as Timespan;
     expect(modelFrequency.targetTimespan, parsedFrequency.targetTimespan);
     expect(modelFrequency.minimumTimespan, parsedFrequency.minimumTimespan);
     expect(modelFrequency.lastEvent, parsedFrequency.lastEvent);
+  });
+  
+  test("Test model with weekday span serialization and deserialization", () async {
+    final model = Reminder(
+      id: Uuid().v1(), 
+      title: Uuid().v1(), 
+      description: Uuid().v1(),
+      frequency: DaysFrequency(
+        days: [DayOfWeek.saturday, DayOfWeek.thursday, DayOfWeek.sunday],
+        lastEvent: DateTime.now(),
+      )
+    );
+
+    final json = model.toJson();
+    print("frequency: ${json['frequency']}");
+    final parsed = Reminder.fromJson(json);
+
+    expect(model.id, parsed.id);
+    expect(model.title, parsed.title);
+    expect(model.description, parsed.description);
+    expect(model.frequency is DaysFrequency, true);
+    final modelFrequency = model.frequency as DaysFrequency;
+    final parsedFrequency = parsed.frequency as DaysFrequency;
+    expect(modelFrequency.days, parsedFrequency?.days);
+    expect(modelFrequency.lastEvent, parsedFrequency?.lastEvent);
   });
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,26 +4,47 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:reminder/data/bloc/database_bloc.dart';
+import 'package:reminder/data/bloc/memory_database_bloc.dart';
+import 'package:reminder/data/database_provider.dart';
+import 'package:reminder/data/model.dart';
+import 'package:reminder/device/platform_provider.dart';
 
 import 'package:reminder/main.dart';
+import 'package:uuid/uuid.dart';
+
+Future testEveryText(WidgetTester tester, MemoryDatabaseBloc database, Widget app) async {
+    await tester.pumpWidget(app);
+    expect(find.text("Loading..."), findsOneWidget);
+    database.update(reminder: Reminder(
+      id: Uuid().v1(),
+      title: Uuid().v1(),
+      description: Uuid().v1(),
+      frequency: Timespan(
+        targetTimespan: 100,
+        minimumTimespan: 80,
+        lastEvent: DateTime.now()
+      )
+    ));
+    await tester.pumpWidget(app);
+    expect(find.text("Loading..."), findsNothing);
+    expect(find.text("every 0 days"), findsOneWidget);
+}
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(new MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Test iOS reminder list', (WidgetTester tester) async {
+    final database = MemoryDatabaseBloc();
+    final app = PlatformProvider(platform: TargetPlatform.iOS, child: MaterialApp(home: DatabaseProvider(child: MyHomePage(), bloc: database)));
+    await testEveryText(tester, database, app);
+  });
+  testWidgets('Test Android reminder list', (WidgetTester tester) async {
+    final database = MemoryDatabaseBloc();
+    final app = PlatformProvider(platform: TargetPlatform.android, child: MaterialApp(home: DatabaseProvider(child: MyHomePage(), bloc: database)));
+    await testEveryText(tester, database, app);
   });
 }
